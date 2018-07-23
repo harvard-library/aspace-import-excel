8b03f12086e80e9f97cbae6a2f0c2282ee763f9c
Bobbi <bobbi_fox@harvard.edu>
2018-07-23 13:15:16 -0400

    change back so I can accept the pull request

--- frontend/routes.rb
+++ frontend/routes.rb
@@ -1,15 +1,11 @@
-# aspace-import-csv routes
 ArchivesSpace::Application.routes.draw do
-  scope AppConfig[:frontend_proxy_prefix] do
-    match 'resources/:rid/getfile' => 'resources_updates#get_file', :via => [:post]
-    match 'resources/:rid/getfile' => 'resources_updates#get_file', :via => [:get]
-    # match 'resources/ssload'  => 'resources_updates#load_ss', :via => [:post]
-    match 'resources/:id/ssload' => 'resources_updates#load_ss', :via => [:post]
-    match 'resources/:id/ssload' => 'resources_updates#load_ss', :via => [:get]
-    match 'resources/:id/getdofile' => 'resources_updates#get_do_file', :via => [:get]
-    match 'resources/:id/getdofile' => 'resources_updates#get_do_file', :via => [:post]
-    match 'resources/:id/digital_load' => 'resources_updates#load_dos', :via => [:get]
-    match 'resources/:id/digital_load' => 'resources_updates#load_dos', :via => [:post]
-  end
+   match 'resources/:rid/getfile' => 'resources_updates#get_file', :via => [:post]
+   match 'resources/:rid/getfile' => 'resources_updates#get_file', :via => [:get]
+#   match 'resources/ssload'  => 'resources_updates#load_ss', :via => [:post]
+   match 'resources/:id/ssload' => 'resources_updates#load_ss', :via => [:post]
+   match 'resources/:id/ssload' => 'resources_updates#load_ss', :via => [:get]
+   match 'resources/:id/getdofile'=> 'resources_updates#get_do_file', :via => [:get]
+   match 'resources/:id/getdofile'=> 'resources_updates#get_do_file', :via => [:post]
+   match 'resources/:id/digital_load' => 'resources_updates#load_dos', :via => [:get]
+   match 'resources/:id/digital_load' => 'resources_updates#load_dos', :via => [:post]
 end
-
